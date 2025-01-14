a82992043cc4a2265cbf37278571c85ad5fbdf86
Atomx3 <atom.x.hooray@gmail.com>
2025-01-14 14:26:22 +0100

    Remove all files except .git directory

--- cleanup.sh
+++
@@ -1,7 +0,0 @@
-# Remove development configs
-sudo rm -f .prettierrc
-sudo rm -f .prettierignore
-sudo rm -f .ruby-version
-sudo rm -f Gemfile.lock
-sudo rm -f package-lock.json
-sudo rm -f bin/deploy
